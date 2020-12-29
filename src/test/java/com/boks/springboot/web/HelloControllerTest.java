1fb0ea75263b9f27ca06e074e2cbbc3afb1268e3
Nam-Ki-Bok <nkb7714@naver.com>
2020-12-29 23:05:48 +0900

    [2020.12.29] helloDto GET API 테스트 추가

--- src/test/java/com/boks/springboot/web/HelloControllerTest.java
+++ src/test/java/com/boks/springboot/web/HelloControllerTest.java
@@ -8,9 +8,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)  // 스프링 부트 테스트와 JUnit 사이의 연결자 역할
@@ -27,5 +28,18 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))  // MockMvc를 통해 /hello 주소로 GET 요청
                 .andExpect(status().isOk())  // 이 요청의 상태를 검증, 이 경우 OK 즉 200 인지 확인
                 .andExpect(content().string(hello));  // GET을 통해 받은 값이 hello 인지 확인
+    }
+
+    @Test
+    public void returnHelloDto() throws Exception {
+        String name = "test";
+        int amount = 100;
+
+        mvc.perform(get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
