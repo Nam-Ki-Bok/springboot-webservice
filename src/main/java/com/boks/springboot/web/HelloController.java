7acf9cd3e9989f9e9566450259c3e83f4f14af5f
Nam-Ki-Bok <nkb7714@naver.com>
2020-12-29 17:06:11 +0900

    [2020.12.29] @RestController, @GetMapping 주석 추가

--- src/main/java/com/boks/springboot/web/HelloController.java
+++ src/main/java/com/boks/springboot/web/HelloController.java
@@ -3,10 +3,10 @@ package com.boks.springboot.web;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController  // 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어 줌.
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/hello")  // GET 요청을 받을 수 있는 API를 만들어 줌.
     public String hello() {
         return "hello";
     }
