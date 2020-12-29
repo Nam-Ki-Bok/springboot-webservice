7f3b70b1529b73cae42ecbf069aef7b8b9b4bccd
Nam-Ki-Bok <nkb7714@naver.com>
2020-12-29 20:40:04 +0900

    [2020.12.29] HelloResponseDto 테스트 코드 추가

---
+++ src/test/java/com/boks/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,21 @@
+package com.boks.springboot.web.dto;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void testLombokFunction() {
+        // Given
+        String name = "test";
+        int amount = 1000;
+
+        // When
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        // Then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
