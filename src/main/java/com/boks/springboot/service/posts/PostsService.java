4564d47faa696dc10217b33ee01f9a273bec3e4e
Nam-Ki-Bok <nkb7714@naver.com>
2021-01-01 22:30:02 +0900

    [2021.01.01] Post 등록 기능 추가

--- src/main/java/com/boks/springboot/service/posts/PostsService.java
+++ src/main/java/com/boks/springboot/service/posts/PostsService.java
@@ -1,4 +1,18 @@
 package com.boks.springboot.service.posts;
 
+import com.boks.springboot.domain.posts.PostsRepository;
+import com.boks.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
 public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
 }
