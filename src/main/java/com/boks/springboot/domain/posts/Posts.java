ceaf164233bd8dc8cc43086f748f69ceca9a7972
Nam-Ki-Bok <nkb7714@naver.com>
2021-01-03 22:32:52 +0900

    [2020.01.03] BaseTimeEntity 상속

--- src/main/java/com/boks/springboot/domain/posts/Posts.java
+++ src/main/java/com/boks/springboot/domain/posts/Posts.java
@@ -1,6 +1,7 @@
 package com.boks.springboot.domain.posts;
 
 
+import com.boks.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,7 @@ import javax.persistence.*;
 @Getter  // 롬복의 Get 생성 어노테이션
 @NoArgsConstructor // 롬복의 기본 생성자 자동 추가 어노테이션
 @Entity  // DB의 테이블과 매칭 될 클래스임을 나타냄
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id  // 해당 테이블의 PK를 나타냄
     @GeneratedValue (strategy = GenerationType.IDENTITY)  // PK auto_increment
     private Long id;
