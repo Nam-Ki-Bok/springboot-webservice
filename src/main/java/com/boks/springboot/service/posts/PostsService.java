9d0900438f8722332bb76d88969b0d5761844adc
Nam-Ki-Bok <nkb7714@naver.com>
2021-01-06 00:31:14 +0900

    [2020.01.05] 전체 글 조회 화면 생성

--- src/main/java/com/boks/springboot/service/posts/PostsService.java
+++ src/main/java/com/boks/springboot/service/posts/PostsService.java
@@ -2,12 +2,15 @@ package com.boks.springboot.service.posts;
 
 import com.boks.springboot.domain.posts.Posts;
 import com.boks.springboot.domain.posts.PostsRepository;
+import com.boks.springboot.web.dto.PostsListResponseDto;
 import com.boks.springboot.web.dto.PostsResponseDto;
 import com.boks.springboot.web.dto.PostsSaveRequestDto;
 import com.boks.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +37,12 @@ public class PostsService {
                 new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
