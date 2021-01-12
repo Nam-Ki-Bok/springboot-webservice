7ee69464b0e069b600bd73c3c3cb235c39533250
Nam-Ki-Bok <nkb7714@naver.com>
2021-01-12 20:16:58 +0900

    [2020.01.12] Google 로그인 기능 추가

--- src/main/java/com/boks/springboot/web/IndexController.java
+++ src/main/java/com/boks/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.boks.springboot.web;
 
+import com.boks.springboot.config.auth.dto.SessionUser;
 import com.boks.springboot.service.posts.PostsService;
 import com.boks.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -7,16 +8,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
